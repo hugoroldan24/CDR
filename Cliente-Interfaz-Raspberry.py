0a80dcefd2a2e25c3e339ba06b3278516fd0a39d
cristobal-roca <cristobal.roca@estudiantat.upc.edu>
2025-04-16 14:21:54 +0200

    Update Cliente-Interfaz-Raspberry.py
    Expansion pantalla login/query +funciones

--- Cliente-Interfaz-Raspberry.py
+++ Cliente-Interfaz-Raspberry.py
@@ -4,6 +4,7 @@ from gi.repository import Gtk, GLib
 
 import threading
 import requests
+import time
 
 from lector import obtener_uid
 
@@ -13,24 +14,72 @@ SERVER_URL = "http://IP_DEL_SERVIDOR:PORT/auth"  # <-- CAMBIAR ESTO CUANDO SEPA 
 class AteneaClient(Gtk.Window):
     def __init__(self):
         super().__init__(title="Client Atenea")
+        self.server = #añadir IP server
+        self.port = #añadir puerto
+        self.timer = None
+        self.inactivity_timeout = 60  #timeout de login para inactividad
+
+
+        
+        #configuracion ventana
         self.set_border_width(10)
         self.set_default_size(400, 300)
+        self.connect("destroy", self.on_destroy) # conecta la señal destroy del objeto(la ventana) a un metodo handler (self.on_destroy)
 
         # Layout principal
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(vbox)
+        self.stack = Gtk.Stack()  
+        self.stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT)
+        self.stack.set_transition_duration(300)
+        self.add(self.stack)
+        
+        self.loginbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.stack.add_named(self.loginbox, "login")
+        self.loginbox.set_margin_top(20)
+        self.loginbox.set_margin_bottom(20)
+        self.loginbox.set_margin_start(20)
+        self.loginbox.set_margin_end(20)
 
         # Info label
-        self.label = Gtk.Label(label="PLEASE, LOGIN WITH YOUR UNIVERSITY CARD")
-        vbox.pack_start(self.label, False, False, 0)
+        self.loginlabel = Gtk.Label(label="PLEASE, LOGIN WITH YOUR UNIVERSITY CARD")
+        self.loginbox.pack_start(self.label, False, False, 0)
+        
+
 
         # TextView per mostrar info del servidor
         self.textview = Gtk.TextView()
         self.textview.set_editable(False)
         vbox.pack_start(self.textview, True, True, 0)
 
+        #Pantalla query
+        self.querybox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.stack.add_named(self.querybox, "query")
+        self.querybox.set_margin_top(10)
+        self.querybox.set_margin_bottom(10)
+        self.querybox.set_margin_start(10)
+        self.querybox.set_margin_end(10)
+        
+        self.welcomelabel = Gtk.Label(label="WELCOME")
+        self.querybox.pack_start(self.label, False, False, 0)
+
+        self.query_entry = Gtk.Entry()                                   #nos permite escribir aquello que ordenamos  
+        self.query_entry.set_placeholder_text("Type your query:")         
+        self.query_entry.connect("activate", self.in_query)               #volvemos a conectar la señal activate al metodo in_query.
+        self.query_box.pack_start(self.query_entry, False, False, 0)
+        
+        
+        self.logout_button = Gtk.Button(label="Logout")                   #ponemos un boton para deslogear
+        self.logout_button.connect("clicked", self.logout)               #conectamos la señal clicked al metodo logout
+        self.query_box.pack_start(self.logout_button, False, False, 0)
+
         # Comença la lectura del UID
         self.start_uid_thread()
+
+        #se inicializa de manera que lo primero que salga sea la login screen
+        self.stack.set_visible_child_name("login")
+
+        #importante hace que todo se vea visible
+        self.show_all()
+
 
     def start_uid_thread(self):
         thread = threading.Thread(target=self.read_uid_loop, daemon=True)
@@ -67,7 +116,28 @@ class AteneaClient(Gtk.Window):
           lines = message.split("\n")
           for i, line in enumerate(lines[:4]):
           lcd.lcd_display_string(line[:20], i + 1)
-        
+
+
+
+      def on_destroy(self, widget):                                   #metodo para cerrar aplicacion.
+        print("Ventana cerrada. Saliendo de la aplicación.")
+        Gtk.main_quit()
+
+      def on_timeout(self):                                          # hace log out y reinicia el timer
+        self.on_logout(None)
+        GLib.source_remove(self.timer)
+        self.timer = None
+
+
+      def on_logout(self, button):                                         #funcion log out, resetea timer, muestra el widjet hijo ventana log in
+        GLib.source_remove(self.timer)
+        self.timer = None
+        self.stack.set_visible_child_name("login")
+        self.login_label.set_text("Please login with your university card")
+
+
+
+
 
 if __name__ == "__main__":
     win = AteneaClient()
