f1395a54565b7a618296bb9b6aafcea422c60982
hugoroldan24 <hugoroldan060@gmail.com>
2025-04-17 14:45:02 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,19 +1,17 @@
 <?php   
    include("classes.php");  
    session_start();
-   
-   $id = $_GET['id'];
-   
+
    $data_base_link = new connexionDB('localhost','dbname','username','password');
-   $login = new LogIn($data_base_link,$id);
+   $login = new LogIn($data_base_link);
    
    $_SESSION['connexion_db'] = $data_base_link;
-   $_SESSION['user_id'] = $id;
+   $_SESSION['user_id'] = $_GET['id'];
    $_SESSION['last_activity'] = time(); //Iniciamos el temporizador de inactividad
    
    $data_base_link->connect(); 
        
-   echo($login->getUsername()); 
+   echo($login->getUsername($_SESSION['user_id'])); 
          						               						           
    $data_base_link->disconnect();
 
