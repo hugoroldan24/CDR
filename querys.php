87f2e5cb6b7233598fa3f55cb2cdfd46606d4934
hugoroldan24 <hugoroldan060@gmail.com>
2025-04-17 13:03:56 +0200

    Update querys.php

--- querys.php
+++ querys.php
@@ -1,3 +1,14 @@
 <?php
-
+   include("classes.php");
+   session_start();
+   CheckInactivityTimer();
+   
+   $data_base_link = $_SESSION['connexion_db'];  
+   $query_manager = new queryManager($data_base_link->$connexion,$_SERVER['REQUEST_URI'],$_SESSION['user_id']); 
+   $query_manager->obtainTable();
+   $query_manager->parseQuery();  
+   $data_base_link->connect();  
+   $query_manager->ConvertQuerytoSQL();  
+   echo($query_manager->ConvertQuerySQLtoClient());   
+   $data_base_link->disconnect();
 ?>
