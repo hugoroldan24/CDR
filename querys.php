78707844bbce3f5939570d4e80f7c57d0db868ce
Alvaro.Martinez <alvaro.martinez.aniento@estudiantat.upc.edu>
2025-04-21 17:26:27 +0200

    Update querys.php

--- querys.php
+++ querys.php
@@ -1,21 +1,6 @@
 <?php
    include("classes.php");
    session_start();
-   CheckInactivityTimer();
-   
-   $data_base_link = $_SESSION['connexion_db'];  
-   $query_manager = new queryManager($data_base_link, $_SERVER['REQUEST_URI'], $_SESSION['user_id']); 
-   $query_manager->obtainTable();
-   $query_manager->parseQuery();  
-   $data_base_link->connect();  
-   $query_manager->ConvertQuerytoSQL();  
-   echo($query_manager->ConvertQuerySQLtoClient());   
-   $data_base_link->disconnect();
-?>
----------------------------------------
-<?php
-   include("classes.php");
-   session_start();
 
    // Verificar si la sesión contiene los datos necesarios
    if (!isset($_SESSION['connexion_db']) || !isset($_SESSION['user_id'])) {
