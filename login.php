dcce1518b93d30ffe66ee0c736d204b4bb6773c2
Alvaro.Martinez <alvaro.martinez.aniento@estudiantat.upc.edu>
2025-04-21 17:22:48 +0200

    Update login.php

--- login.php
+++ login.php
@@ -2,26 +2,6 @@
    include("classes.php");  
    session_start();
 
-   $data_base_link = new connexionDB('localhost','dbname','username','password');
-   $login = new LogIn($data_base_link);
-   
-   $_SESSION['connexion_db'] = $data_base_link;
-   $_SESSION['user_id'] = $_GET['id'];
-   $_SESSION['last_activity'] = time(); //Iniciamos el temporizador de inactividad
-   
-   $data_base_link->connect(); 
-       
-   echo($login->getUsername($_SESSION['user_id'])); 
-         						               						           
-   $data_base_link->disconnect();
-
-?>
-
------------------------------------------
-<?php   
-   include("classes.php");  
-   session_start();
-
    // Validar si el parámetro 'id' está presente en la URL
    if (!isset($_GET['id']) || empty($_GET['id'])) {
        die("ID de usuario no proporcionado.");
@@ -30,7 +10,7 @@
    // Sanitizar el parámetro 'id' para mayor seguridad
                                                          //$user_id = intval($_GET['id']); // Convertir a entero para evitar inyecciones SQL
    $user_id = $_GET['id'];
-   $data_base_link = new connexionDB('localhost', 'dbname', 'username', 'password');
+   $data_base_link = new connexionDB('192.168.1.49', 'Afrodita', 'alvaro', 'afrodita');
    $login = new LogIn($data_base_link);
    
    $_SESSION['connexion_db'] = $data_base_link;
