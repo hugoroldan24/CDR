f526a73b8216c97294f510708d166aa5e8970fba
hugoroldan24 <hugoroldan060@gmail.com>
2025-04-22 12:33:46 +0200

    Update login.php

--- login.php
+++ login.php
@@ -6,17 +6,13 @@
    // Validar si el parámetro 'id' está presente en la URL
    if (!isset($_GET['id']) || empty($_GET['id'])) {
        die("ID de usuario no proporcionado.");
-   }
-
-   // Sanitizar el parámetro 'id' para mayor seguridad
-                                                        
+   }                                                        
    $user_id = $_GET['id'];
    $connexion = new mysqli('192.168.1.49','Afrodita','alvaro','afrodita);   
    if ($connexion->connect_error) {
          die("Connexió fallida " . $connexion->connect_error);
    }   
-   $login = new LogIn($connexion);   
-   $_SESSION['connexion_db'] = $connexion;
+   $login = new LogIn($_SESSION['connexion_db']);   
    $_SESSION['user_id'] = $user_id;
    $_SESSION['last_activity'] = time(); // Iniciar el temporizador de inactividad       
    echo($login->getUsername($_SESSION['user_id']));                                              						           
