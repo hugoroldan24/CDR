eb440d9fe6d849233ebca03399b2716e2be20a2f
hugoroldan24 <hugoroldan060@gmail.com>
2025-04-22 12:54:13 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,6 @@
 <?php
-session_start();
 $connexion = new mysqli('192.168.1.49','Afrodita','alvaro','afrodita);   
    if ($connexion->connect_error) {
          die("Connexió fallida " . $connexion->connect_error);
    } 
-$_SESSION['connexion_db'] = $connexion;
 ?>
