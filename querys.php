7f247fad6b46e266a9785957964f8bb4b42ac25b
hugoroldan24 <hugoroldan060@gmail.com>
2025-04-22 12:26:09 +0200

    Update querys.php

--- querys.php
+++ querys.php
@@ -11,16 +11,17 @@
    CheckInactivityTimer();
 
    // Recuperar la conexión desde la sesión
-   $data_base_link = $_SESSION['connexion_db'];  
+   $connexion = $_SESSION['connexion_db'];  
 
    // Crear una instancia de queryManager
-   $query_manager = new queryManager($data_base_link->connexion, $_SERVER['REQUEST_URI'], $_SESSION['user_id']); 
+   $query_manager = new queryManager($connexion, $_SERVER['REQUEST_URI'], $_SESSION['user_id']); 
 
    // Ejecutar las operaciones necesarias
    $query_manager->obtainTable();
-   $query_manager->parseQuery();  
-   $data_base_link->connect();  
+   $query_manager->parseQuery();
+ 
    $query_manager->ConvertQuerytoSQL();  
    echo($query_manager->ConvertQuerySQLtoClient());   
-   $data_base_link->disconnect();
+
+   $connexion->close();
 ?>
