7d95811f54792e71e242aef9ff9280c295f8d165
hugoroldan24 <hugoroldan060@gmail.com>
2025-05-04 13:20:40 +0200

    Update login.php

--- login.php
+++ login.php
@@ -3,10 +3,10 @@ session_start();
 include("clases.php");
 
 // Validar y sanitizar el ID
-if (!isset($_GET['id']) || empty($_GET['id'])) {
-    http_response_code(400);
-    die(json_encode(['status' => 'error', 'message' => 'ID de usuario no proporcionado.']));
-}
+//if (!isset($_GET['id']) || empty($_GET['id'])) {
+//    http_response_code(400);
+//    die(json_encode(['status' => 'error', 'message' => 'ID de usuario no proporcionado.']));
+//}
 
 $user_id = $_GET['id'];
 include("connect.php");
