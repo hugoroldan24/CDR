f76829c3652ae468350787ed4e7d1ec1809428d1
hugoroldan24 <hugoroldan060@gmail.com>
2025-05-04 12:36:23 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,24 +1,18 @@
 <?php
 session_start();
 include("clases.php");
-
-
 // Validar y sanitizar el ID
 if (!isset($_GET['id']) || empty($_GET['id'])) {
     http_response_code(400);
     die(json_encode(['status' => 'error', 'message' => 'ID de usuario no proporcionado.']));
 }
-
 $user_id = $_GET['id'];
 echo $user_id; //Esto es para debugar y ver si la ID es nula o no.
 include("connect.php");
-
 $login = new LogIn($connexion);
 $_SESSION['user_id'] = $user_id;
 $_SESSION['connexion'] = true;
 $_SESSION['last_activity'] = time();
-
 $login->getUsername($user_id);
-
 $connexion->close();
 ?>
