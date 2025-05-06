7e4babb77a6d6e4e5f6dd5784fcca2d1f6dca1ce
hugoroldan24 <hugoroldan060@gmail.com>
2025-05-06 16:20:08 +0200

    Update querys.php

--- querys.php
+++ querys.php
@@ -9,6 +9,7 @@ CheckInactivityTimer();
 // Luego verificar sesión
 if (!isset($_SESSION['user_id'])) {
     http_response_code(401);
+     header('Content-Type: application/json');
     die(json_encode(['status' => 'error', 'message' => 'Sesión no válida o expirada.']));
 }
 
@@ -16,14 +17,14 @@ include("connect.php");
 try {
     $query_manager = new queryManager($connexion, $_SERVER['REQUEST_URI'], $_SESSION['user_id']);
 
-    $query_manager->obtainTable();
-    $query_manager->parseQuery();
+    $query_manager->ParseQuery();
     $query_manager->ConvertQuerytoSQL();
 
     header('Content-Type: application/json');
     echo $query_manager->ConvertQuerySQLtoClient();
 } catch (Exception $e) {
     http_response_code(500);
+    header('Content-Type: application/json');
     die(json_encode(['status' => 'error', 'message' => 'Error en el servidor: ' . $e->getMessage()]));
 }
 
