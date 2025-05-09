ae9d58f7decfc0af661b7ff097fd99de10fce8a4
hugoroldan24 <hugoroldan060@gmail.com>
2025-05-09 15:11:58 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
-session_start();
-
+session_start(); //Si el Cliente no envia ninguna cookie (process_uid, aun no hemos iniciado sesion), esto te genera una nueva PHPSESSID que se devolverá al cliente. Si al hacer esto, tenemos una cookie existente (en querys.php) accederemos a las variablas de sesion guardadas.
+session_regenerate_id(true) //Genera un nuevo identificador de sesion (PHPSESSID) para que el anterior deje de ser válido
 $user_id = $_GET['id'];
 include("connect.php");
 include("clases.php");
